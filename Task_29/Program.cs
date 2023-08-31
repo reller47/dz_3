9985d7f439c9fcf69a6b5a24a863790ddd06f015
Kirill Mikhaylov <tytkaktyt463@gmail.com>
2023-09-01 01:26:01 +0300

    Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,8 @@
+﻿double[] array = new double[8];
+Random rnd = new Random();
+for (int i = 0; i < 8; i++){
+    array[i] = rnd.Next(100);
+}
+for (int i = 0; i < 8; i++){
+    Console.Write(array[i] + "  ");
+}
