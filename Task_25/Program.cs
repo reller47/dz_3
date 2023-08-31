4d3b6d0457cd25fd86ef045be2eeaca9534bae87
Kirill Mikhaylov <tytkaktyt463@gmail.com>
2023-09-01 01:30:03 +0300

    Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Enter A = ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter B = ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Result = " + Math.Pow(a, b));
