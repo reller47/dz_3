386b0ee7470e3ac2db145b4d0e35fa8f3b7229b4
Kirill Mikhaylov <tytkaktyt463@gmail.com>
2023-09-01 01:30:20 +0300

    Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter = ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 10;
+int c = 1;
+int i = 1;
+int sum = 0;
+while ((a/b) != 0){
+    b = b * 10;
+    c++;
+}
+while (i <= c){
+sum = sum + (a % 10);
+a = a / 10;
+i++;
+}
+Console.WriteLine("Result = " + sum);
